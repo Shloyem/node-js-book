324724c0ed10e197aaf37681127cfa5b750e95b3
shloyem <39220943+Shloyem@users.noreply.github.com>
2023-09-24 21:48:28 +0300

    Add linting with eslint AirBnb standard example

---
+++ 12_test_examples/7_linting_eslint/app-post.js
@@ -0,0 +1,4 @@
+// run './node_modules/.bin/eslint ./12_test_examples/7_linting_eslint/app-post.js'
+const theAnswer = 42;
+/* eslint-disable-next-line no-console */
+console.log(`The Answer to the Ultimate Question of Life, the Universe, and Everything is ${theAnswer}`);
---
+++ 12_test_examples/7_linting_eslint/app-pre.js
@@ -0,0 +1,5 @@
+// Text that intentionally has issues for linter to mention
+// run './node_modules/.bin/eslint ./12_test_examples/7_linting_eslint/app-pre.js'
+var the_answer = 42;
+console.log('The Answer to the Ultimate Question of Life, the Universe, and Everything is ' + the_answer);
+// fixed version in app-post.js
