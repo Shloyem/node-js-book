90e9f1d8e165d7503207a62bd43a2953b5bec43f
shloyem <39220943+Shloyem@users.noreply.github.com>
2023-09-29 15:00:36 +0300

    Add stub method override example

--- 12_test_examples/4_mocking_sinon/3-stubs.js
+++ 12_test_examples/4_mocking_sinon/3-stubs.js
@@ -1,7 +1,6 @@
 // run with "node .\12_test_examples\4_mocking_sinon\3-stubs.js"
 
 // stub - allows returning different values according to the arguments and called amount.
-
 const sinon = require('sinon');
 
 const stub = sinon.stub();
@@ -17,3 +16,14 @@ for (let index = 0; index < 20; index++) {
   stubForCallsAmount.onCall(index).returns(index + 1);
   console.log(`Call #${index + 1} returned: ${stubForCallsAmount()}`);
 }
+
+// stub allows to override a method inside an object, useful for overriding external modules dependencies
+const obj = {
+  greet: () => 'hello!'
+};
+const stubMethodOveride = sinon.stub(obj, 'greet');
+stub.onFirstCall().returns('hi!');
+stub.onSecondCall().returns('howdy!');
+console.log(obj.greet()); // Outputs: 'hi!'
+console.log(obj.greet()); // Outputs: 'howdy!'
+
