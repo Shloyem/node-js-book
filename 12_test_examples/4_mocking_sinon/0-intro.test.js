93418a8c3ecac9fda1396e37c2d0ed701f6ca049
shloyem <39220943+Shloyem@users.noreply.github.com>
2023-10-31 13:24:28 +0200

    Add stub and spy example

--- 12_test_examples/4_mocking_sinon/0-intro.test.js
+++ 12_test_examples/4_mocking_sinon/0-intro.test.js
@@ -11,10 +11,24 @@ describe('stub demo', () => {
     // arrange
     const { promises: fs } = require('fs');
     const { getLineCount } = require('./line-count');
-    sandbox.stub(fs, 'readFile').resolves('line1\nline2')
+    sandbox.stub(fs, 'readFile').resolves('line1\nline2') // readFile returns a promise - so its resolves and not returns
     // act
     const actual = await getLineCount(null);
     // assert
     assert.strictEqual(actual, 2);
+  });
+});
+
+describe('spy demo', () => {
+  it('should use utf-8 for reading the file', async () => {
+    // arrange
+    const { promises: fs } = require('fs');
+    const { getLineCount } = require('./line-count');
+    const stub = sandbox.stub(fs,
+      'readFile').resolves('line1\nline2')
+    // act
+    await getLineCount(null);
+    // assert
+    stub.calledOnceWithExactly(null, 'utf-8');
   });
 });
